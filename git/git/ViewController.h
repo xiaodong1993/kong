b54fa583982de2545a605194f058b0c0100ec122
qianfeng <qianfeng@www.local>
2016-04-28 11:15:38 +0800

    创建

---
+++ git/git/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  git
+//
+//  Created by qianfeng on 16/4/28.
+//  Copyright (c) 2016年 wds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ git/git/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  git
+//
+//  Created by qianfeng on 16/4/28.
+//  Copyright (c) 2016年 wds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
